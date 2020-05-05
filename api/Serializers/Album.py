2377054b93026f0c73b6cb5d0da080f4a497ecd6
amir <amirmohhamadsoltani@gmail.com>
2020-05-05 23:13:33 +0430

    config for liyara

--- api/Serializers/Album.py
+++ api/Serializers/Album.py
@@ -1,9 +1,11 @@
 from api.models import Albums
 from rest_framework import serializers as s
+from django.conf import settings
 
 
 class AlbumSerializer(s.ModelSerializer):
-    pictures = s.SerializerMethodField(read_only=True, method_name="get_images")
+    pictures = s.SerializerMethodField(
+        read_only=True, method_name="get_images")
 
     class Meta:
         model = Albums
@@ -12,7 +14,7 @@ class AlbumSerializer(s.ModelSerializer):
 
     def get_images(self, obj) -> list:
         images: list = obj.pictures_set.all()[:4].values_list("img", flat=True)
-        return [self.context['request'].get_host() + "/" + image for image in images]
+        return ["http://"+self.context['request'].get_host() + settings.MEDIA_URL + image for image in images]
 
     def create(self, validated_data):
         album = Albums(author=self.context['request'].user, **validated_data)
--- gallery/settings.py
+++ gallery/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'iwyi-1lsh8j$64k4%%*gk26u$lh1=+zfjw7x)t*6xlx&gy()1j'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", True)
 
 ALLOWED_HOSTS = []
 
@@ -54,8 +54,7 @@ ROOT_URLCONF = 'gallery.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
-        ,
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -113,7 +112,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
+STATIC_ROOT = "static/"
 STATIC_URL = '/static/'
-MEDIA_ROOT='media/'
-MEDIA_URL='/media/'
+MEDIA_ROOT = 'media/'
+MEDIA_URL = '/media/'
