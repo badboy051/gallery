5d46adf027c774e3850f2a40a8448f9985df2d67
amir <amirmohhamadsoltani@gmail.com>
2020-06-01 21:27:03 +0430

    update api

--- gallery/settings.py
+++ gallery/settings.py
@@ -130,3 +130,15 @@ STATIC_URL = '/static/'
 MEDIA_ROOT = 'media/'
 MEDIA_ROOT = 'media/'
 MEDIA_URL = '/media/'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ),
+}
